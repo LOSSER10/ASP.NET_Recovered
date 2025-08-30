45f2921be475ac14042ff23eaaaa57deae320b5c
MD Leon <114634496+LOSSER10@users.noreply.github.com>
2025-08-30 12:47:06 +0600

    Add files via upload
    Added Task_2

---
+++ Assignment_Task2_Task3/Task2/Class_leon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_Task1_Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] number = new int[5];
+            Console.WriteLine("Enter 5 numbers:");
+
+            for (int i = 0; i < 5; i++)
+            {
+                number[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("The numbers you entered are:");
+
+            foreach (int num in number)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine("\n");
+
+        }
+    }
+}
+
